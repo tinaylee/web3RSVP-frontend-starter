b5bef1210c50ecce3140b47e22ea70f1c4fbba4f
Firstname Lastname <e-mail address>
2022-08-10 14:53:56 -0700

    Connected contract with Ethers.js

---
+++ utils/connectContract.js
@@ -0,0 +1,24 @@
+import abiJSON from "./Web3RSVP.json"
+import { ethers } from "ethers";
+
+function connectContract() {
+    const contractAddress = "0xDb3122A3850206c1A5838BB2091bbD43E1d740C4";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            rsvpContract = new ethers.Contract(contractAddress, contractABI, signer);
+        } else {
+            console.log("Ethereum object doesn't exist!");
+        }
+    } catch (error) {
+        console.log("ERROR:", error);
+    }
+    return rsvpContract;
+}
+
+export default connectContract;
