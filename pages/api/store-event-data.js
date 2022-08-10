972ab063d981d37e5fe99c1c4ae81013e37bb7d2
Firstname Lastname <e-mail address>
2022-08-10 15:16:38 -0700

    updated comments

--- pages/api/store-event-data.js
+++ pages/api/store-event-data.js
@@ -1,11 +1,12 @@
 import { Web3Storage, File, getFilesFromPath } from "web3.storage";
 const { resolve } = require("path");
 
-/** Create a handler function to handle incoming requests to store data
- * POST method - creates a new record.  Check if incoming request is POST request and 
- * create a new event if it is.
-*/
+// Create a handler function to handle incoming requests to store data
+
+//export default exposes function to other modules
 export default async function handler(req, res) {
+ /*  POST method - creates a new record.  Check if incoming request is POST request and 
+ create a new event if it is.*/
     if (req.method == "POST") {
         return await storeEventData(req, res);
     } else {
@@ -27,8 +28,9 @@ async function storeEventData(req, res) {
         .json({ error: "Error creating event", success: false });
     }
 }
-
+//creates JSON file that includes metadata passed in from req.body
 async function makeFileObjects(body) {
+//protocol buffers serialize objects (converts them to bytes) to transmit them to another machine
     const buffer = Buffer.from(JSON.stringify(body));
     
     const imageDirectory = resolve(process.cwd(), `public/images/${body.image}`);
@@ -37,15 +39,15 @@ async function makeFileObjects(body) {
     files.push(new File([buffer], "data.json"));
     return files;
 }
-
+//creates web3Storage client
 function makeStorageClient() {
     return new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN });
 }
-
+//stores JSON file from req.body in Web3Storage
 async function storeFiles(files) {
     const client = makeStorageClient();
     const cid = await client.put(files);
     return cid;
 }
-
+//cid is content identifier in the form of a hash.  Will be stored on chain and used for file retrieval
 
