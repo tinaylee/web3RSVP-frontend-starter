060665960acf145bb3d3bbf7219634a57727d473
Firstname Lastname <e-mail address>
2022-08-11 10:18:16 -0700

    Fixed errors and confirmed page loads

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvier } from "wagmi/providers/public";
+import { publicProvider } from "wagmi/providers/public";
 
 const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
 
@@ -12,12 +13,12 @@ const {chains, provider } = configureChains(
   [alchemyProvider({ alchemyId }), publicProvider()]
 );
 
-const { connectors } = getDefaultWAllets({
+const { connectors } = getDefaultWallets({
   appName: 'web3rsvp',
   chains,
 });
 
-const wagmiClient = createClient = createClient({
+const wagmiClient = createClient({
   autoConnect: true,
   connectors,
   provider,
